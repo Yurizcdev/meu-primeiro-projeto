a2b0912472c0ce268db2b07ddb52f3b3d19dcf80
Yurizcdev <yurizcrestani@gmail.com>
2026-04-09 20:17:31 -0300

    Fix syntax errors in arithmetic functions

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 alert("JS funcionando");
+
 function mostrarMensagem() {
     alert("Bem-vindo ao meu portfólio!");
 }
-function somar() function subtrair() {
+
+function somar() {
+    let n1 = document.getElementById("num1").value;
+    let n2 = document.getElementById("num2").value;
+    document.getElementById("resultado").innerText = Number(n1) + Number(n2);
+}
+
+function subtrair() {
     let n1 = document.getElementById("num1").value;
     let n2 = document.getElementById("num2").value;
     document.getElementById("resultado").innerText = Number(n1) - Number(n2);
@@ -18,8 +26,4 @@ function dividir() {
     let n1 = document.getElementById("num1").value;
     let n2 = document.getElementById("num2").value;
     document.getElementById("resultado").innerText = Number(n1) / Number(n2);
-} {
-    let n1 = document.getElementById("num1").value;
-    let n2 = document.getElementById("num2").value;
-    document.getElementById("resultado").innerText = Number(n1) + Number(n2);
 }
